0eb5d8cf598b274caeeb29b4c5159b5e93151f16
phukon <rikiphukon16@gmail.com>
2023-03-02 01:56:03 +0530

    created pages and built the router

--- project-athena-react/src/App.js
+++ project-athena-react/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Layout from './Layout';
+import IndexPage from "./components/IndexPage";
+import LoginPage from './components/LoginPage';
+import ResourcePage from './components/ResourcePage';
+import EventsPage from './components/EventsPage';
+import CommunityPage from './components/CommunityPage';
+import RegisterPage from './components/RegisterPage';
+import { UserContextProvider } from './UserContext';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+         <Routes>
+          <Route path='/' element={<Layout/>}>
+              <Route index element={<IndexPage/>}/>
+              <Route path='/login' element = {<LoginPage/>}/>
+              <Route path='/register' element = {<RegisterPage/>}/>
+              <Route path='/resources' element = {<ResourcePage/>}/>
+              <Route path='/events' element = {<EventsPage/>}/>
+              <Route path='/community' element = {<CommunityPage/>}/>
+          </Route>
+        </Routes>
+    </UserContextProvider>
   );
 }
 
