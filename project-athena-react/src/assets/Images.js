96b789a4f9358317d7869b693ec8fba312248350
phukon <rikiphukon16@gmail.com>
2023-03-03 16:19:31 +0530

    fixed named exports of images

--- project-athena-react/src/assets/Images.js
+++ project-athena-react/src/assets/Images.js
@@ -3,15 +3,19 @@ import elon from '../assets/elon.png';
 import elon2 from '../assets/elon2.png';
 import elon3 from '../assets/elon3.png';
 import elon4 from '../assets/elon4.png';
+import elon5 from '../assets/elon5.png';
 import downArrow from '../assets/down-arrow.svg';
 
 
 
-export default {
+const Images = {
     projectAthena,
     elon,
     elon2,
     elon3,
     elon4,
+    elon5,
     downArrow
 };
+
+export default Images
--- project-athena-react/src/components/IndexPage.js
+++ project-athena-react/src/components/IndexPage.js
@@ -16,7 +16,7 @@ export default function IndexPage() {
             <article id="article-2">
                 <div id="scroll">
                     <p>Scroll</p>
-                    <img src={Images.downArrow}/>
+                    <img src={Images.downArrow} alt=""/>
                 </div>
                 <div id="content-2">
                     <p>
@@ -26,7 +26,7 @@ export default function IndexPage() {
             </article>
             <article id="article-3">
                 <div className="cool-box" id="cool-box-1">
-                    <img src={Images.elon2}/>
+                    <img src={Images.elon2} alt=""/>
                     <h1>Study Smarter, Not Harder</h1>
                     <p>Share and Discover Resources for Your Academic Success</p>
                     <button>Explore</button>
@@ -34,7 +34,7 @@ export default function IndexPage() {
             </article>
             <article id="article-4">
                 <div className="cool-box" id="cool-box-2">
-                    <img src={Images.elon3}/>
+                    <img src={Images.elon3} alt=""/>
                     <h1>Academic Extravaganza</h1>
                     <p>Get Excited for Upcoming Events and Opportunities</p>
                     <button>Explore</button>
@@ -42,14 +42,14 @@ export default function IndexPage() {
             </article>
             <article id="article-5">
                 <div className="cool-box" id="cool-box-3">
-                    <img src={Images.elon3}/>
+                    <img src={Images.elon3} alt=""/>
                     <h1>The A-Team</h1>
                     <p>Join a Thriving Community of High Achieving Students</p>
                     <button>Explore</button>
                 </div>
             </article>
-            <article id="article-6">
-                <img src={Images.elon4}/>
+            <article class="article-6">
+                <img src={Images.elon4} alt=""/>
                 <h1>Your Contribution Matters</h1>
                 <p>Help Us Create a Resource Hub That Rocks!</p>
                 <button>Join Now➡</button>
