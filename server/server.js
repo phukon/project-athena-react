b75cf1b1eae246f33598d9a0d4e6a555a0777c54
phukon <rikiphukon16@gmail.com>
2023-04-20 20:11:28 +0530

    cors and corsOptions setup

--- server/server.js
+++ server/server.js
@@ -3,12 +3,20 @@ const app = express();
 const path = require('path');
 const {logger} = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+
 const PORT = process.env.PORT || 3500;
 
+
 app.use(logger)
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.use(express.static(path.join(__dirname, 'public'))); //Overall, this code snippet allows the web application to serve static files to the client without having to write individual routes for each file.
-
 app.use('/', require('./routes/root')); //This code snippet allows the web application to serve the index.html file to the client when the client requests the root URL.
 
 app.all('*', (req, res) => {
@@ -21,7 +29,6 @@ app.all('*', (req, res) => {
         res.type('txt').send('Not found');
     }
 })
-
 
 
 app.use(errorHandler)
