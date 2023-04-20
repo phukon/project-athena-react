ab117088ce6d93d100185e262b499e4d932633c0
phukon <rikiphukon16@gmail.com>
2023-04-20 21:27:48 +0530

    mongoose setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
+
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path');
-const {logger} = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const  connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+const {logger, logEvents} = require('./middleware/logger');
+
+console.log(process.env.NODE_ENV)
+
 
 const PORT = process.env.PORT || 3500;
 
@@ -32,6 +39,13 @@ app.all('*', (req, res) => {
 
 
 app.use(errorHandler)
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)});
+})
+
+mongoose.connection.on('error', err => {
+    console.log(`Error connecting to MongoDB: ${err}`)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
