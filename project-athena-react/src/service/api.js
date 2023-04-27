00162c9072cdcb8cecd746b5e84af04b969bd1f2
phukon <rikiphukon16@gmail.com>
2023-04-27 23:48:52 +0530

    env variable

--- project-athena-react/src/service/api.js
+++ project-athena-react/src/service/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-
-const API_URI = 'http://localhost:3500';
+import * as dotenv from 'dotenv'
+dotenv.config();
 
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(`${API_URI}/files`, data);
+        const response = await axios.post(`${process.env.API_URI}/files`, data);
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
