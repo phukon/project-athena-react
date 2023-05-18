6275b60d8cbaa8e26cf51760be9297389326bc7d
phukon <rikiphukon16@gmail.com>
2023-05-18 22:02:17 +0530

    post request made in axios

--- project-athena-react/src/service/api.js
+++ project-athena-react/src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(``, data);
+        const response = await axios.post(`https://jbb9mv51.api.sanity.io/v2021-10-21/assets/files/production`, data);
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
