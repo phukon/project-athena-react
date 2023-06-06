24438df9853186ee96fb85778ff0fadaa72dc5a9
phukon <rikiphukon16@gmail.com>
2023-06-06 15:28:44 +0530

    evetns posts and page

--- project-athena-react/src/components/EventsPage.js
+++ project-athena-react/src/components/EventsPage.js
@@ -1,8 +1,53 @@
-import React from 'react';
-
+import { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom'
 import Images from '../assets/Images';
+import {fetchEventPosts} from '../service/fetchEventPosts';
 
 export default function EventsPage() {
+
+  const [renderData, setRenderData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getFiles = async () => {
+    const returnedData = await fetchEventPosts();
+
+    if (returnedData.length === 0) {
+      setRenderData(<p>We are gathering the resources...</p>);
+    } else {
+      const renderedItems = returnedData.map((item, index) => {
+        // Resize the image URL
+        const resizedImageUrl = `${item.imageUrl}?fit=fillmax&w=600&h=600`;
+
+        return (
+          <div className='square-box' key={index}>
+            <Link to={`/events/${item.slug.current}`}>
+              <h3>{item.title}</h3>
+              <div id="dateAndUser">
+                <span>{item.postedAt}</span>
+                <span>by Admin</span>
+              </div>
+              <div className="imgDesc">
+              <div className="imgContainer">
+                <img src={resizedImageUrl} alt="" className="small-image" />
+              </div>
+              <div className="descriptionContainer">
+                <p>{item.description}</p>
+              </div>
+             </div>
+          </Link>
+        </div>
+        );
+      });
+      setRenderData(renderedItems);
+    }   
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getFiles();
+  }, []);
+
   return (
     <main>
       <article id='event-article-1'>
@@ -12,14 +57,8 @@ export default function EventsPage() {
 
       <article id='event-article-2'>
         <div id='event-data'>
-          <div className='square-box'>
-            <div>
-              <h3>A talk on marketing.</h3>
-            </div>
-            <div id='details'>
-              <span>3 March 2023</span><span>JIST</span>
-            </div>
-          </div>
+          
+          {isLoading ? <p>Loading...</p> : renderData}
 
           <div className='square-box'>
             <div>
@@ -30,14 +69,6 @@ export default function EventsPage() {
             </div>
           </div>
 
-          <div className='square-box'>
-            <div>
-              <h3>A talk on marketing.</h3>
-            </div>
-            <div id='details'>
-              <span>3 March 2023</span><span>JIST</span>
-            </div>
-          </div>
         </div>
       </article>
 
---
+++ project-athena-react/src/service/fetchEventPosts.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const fetchEventPosts = async () => {
+
+    const projectId = 'jbb9mv51';
+    const dataset = 'production';
+
+    try {
+        let QUERY = encodeURIComponent(
+          `*[_type == "eventpost"]{title, roles, slug, college, body, description, postedAt, "imageUrl": mainImage.asset->url}`
+        );
+
+        const response = await axios.get(
+          `https://${projectId}.api.sanity.io/v2021-10-21/data/query/${dataset}?query=${QUERY}`
+        );
+        return response.data.result;
+      } catch (error) {
+        console.log('Error while calling the API:', error.message);
+        return null;
+      }
+}
